5d15503bcd6553a2a30cae0b20d06f4f2e630442
nishanth28 <pnishanth28@gmail.com>
2017-01-17 21:02:47 -0500

    Added Bird Scene

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -17,7 +17,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     
     enum trackZpositions: CGFloat{
         case background = 0
-        case ground = 1
+        case floor = 1
         case pipes = 2
         case bird = 3
         case labelNodes = 4
@@ -31,8 +31,8 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     var gameOver:Bool = true
     var score : Int = 0
     var scoreLabelNode = SKLabelNode()
-    var FlappyBirdLabelNode = SKLabelNode()
-    var TapStatusNode = SKLabelNode()
+    var flappyBirdLabelNode = SKLabelNode()
+    var tapStatusNode = SKLabelNode()
     var lastUpdateTime: TimeInterval = 0
     
     
@@ -43,6 +43,10 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         
         self.addChild(movingGameObjects)
         createBackground()
+        birdToScene()
+        FloorToScene()
+        ceilToScene()
+        labelsToScene()
     }
     
     func createBackground(){
@@ -67,6 +71,101 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         
     }
     
+    func birdToScene(){
+        
+        let birdTexture =  SKTexture(imageNamed:"bird1")
+        bird = SKSpriteNode(texture: birdTexture)
+        bird.position = CGPoint(x:0,y:0)
+        bird.zPosition = trackZpositions.bird.rawValue
+     bird.run(SKAction.init(named:"birdFly")!,withKey:"Fly")
+        bird.physicsBody = SKPhysicsBody(circleOfRadius:bird.size.height/2)
+        bird.physicsBody?.categoryBitMask = birdMask
+        bird.physicsBody?.contactTestBitMask = openingMask | enemyMask
+        bird.physicsBody?.collisionBitMask = enemyMask
+        
+        
+        bird.physicsBody?.allowsRotation = false
+        bird.physicsBody?.isDynamic = false
+        
+        addChild(bird)
+        
+    }
+    
+    func FloorToScene(){
+        
+        let floor = SKNode()
+        
+        floor.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width:self.frame.width,height:1))
+        floor.physicsBody?.isDynamic = false
+        floor.position = CGPoint(x:0,y:-self.frame.height/2)
+        floor.zPosition = trackZpositions.floor.rawValue
+        floor.physicsBody?.categoryBitMask = enemyMask
+        floor.physicsBody?.contactTestBitMask = birdMask
+        floor.physicsBody?.collisionBitMask = birdMask
+        
+        addChild(floor)
+        
+    }
+    
+    func ceilToScene(){
+        
+        let ceil = SKNode()
+        
+        ceil.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width:self.frame.width,height:1))
+        ceil.physicsBody?.isDynamic = false
+        ceil.position = CGPoint(x:0,y: self.frame.height/2)
+        ceil.zPosition = trackZpositions.floor.rawValue
+        ceil.physicsBody?.categoryBitMask = enemyMask
+        ceil.physicsBody?.contactTestBitMask = birdMask
+        ceil.physicsBody?.collisionBitMask = birdMask
+        
+        addChild(ceil)
+        
+    }
+    
+    func labelsToScene(){
+        
+        flappyBirdLabelNode = SKLabelNode(fontNamed:"Noteworthy-light")
+        flappyBirdLabelNode.fontSize = 100
+        flappyBirdLabelNode.text = "Flappy Bird"
+        flappyBirdLabelNode.position = CGPoint(x:0,y:bird.position.y+100)
+        flappyBirdLabelNode.zPosition = trackZpositions.labelNodes.rawValue
+        addChild(flappyBirdLabelNode)
+        
+        tapStatusNode = SKLabelNode(fontNamed: "HoeflerText-Italic")
+        tapStatusNode.fontSize = 40
+        tapStatusNode.text = "Tap to let the bird fly"
+        tapStatusNode.position = CGPoint(x:0,y:bird.position.y-100)
+        addChild(tapStatusNode)
+        
+        tapStatusNode.run(SKAction.init(named:"Pulse")!)
+        
+        
+        
+    }
+
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        playGame()
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let rotateDown = SKAction.rotate(toAngle:-0.1,duration:0)
+        bird.run(rotateDown)
+    }
+    
+    func playGame(){
+        
+        bird.physicsBody?.velocity = CGVector(dx:0,dy:0)
+        bird.physicsBody?.applyImpulse(CGVector(dx:0,dy:60))
+        let rotateUp = SKAction.rotate(toAngle:0.2,duration:0)
+        bird.run(rotateUp)
+        
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         
     }
@@ -74,6 +173,7 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
         
     }
+    
     
     
 }
